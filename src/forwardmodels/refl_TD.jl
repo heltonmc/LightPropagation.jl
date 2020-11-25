873d6af3aa56d410c9e8abdbf5a478ab6fa85927
Michael Helton <57471570+heltonmc@users.noreply.github.com>
2020-11-24 22:45:58 -0500

    update notation

--- src/forwardmodels/refl_TD.jl
+++ src/forwardmodels/refl_TD.jl
@@ -138,8 +138,8 @@ function DT_transslab(t, β, ρ, s)
 	ν = 29.9792345/nmed
 
     m = Vector(-10:1:10)
-    z₁ₘ = (1 .- 2m).*s .- 4m.*ze .- zs
-    z₂ₘ = (1 .- 2m).*s .- (4m .- 2).*ze .+ zs
+    z1m = (1 .- 2m).*s .- 4m.*ze .- zs
+    z2m = (1 .- 2m).*s .- (4m .- 2).*ze .+ zs
     
 
 
@@ -147,7 +147,7 @@ function DT_transslab(t, β, ρ, s)
     Rt1 = @. Rt1/(2*(4π*D*ν)^(3/2)*t^(5/2))
 
     
-    Rt2 = @. z₁ₘ'*exp(-(z₁ₘ'^2 / (4D*ν*t))) - z₂ₘ'*exp(-(z₂ₘ'^2 / (4D*ν*t)))
+    Rt2 = @. z1m'*exp(-(z1m'^2 / (4D*ν*t))) - z2m'*exp(-(z2m'^2 / (4D*ν*t)))
     Rt21 = sum(Rt2, dims=2)
     
     
@@ -158,13 +158,6 @@ function DT_transslab(t, β, ρ, s)
     replace!(Rt, NaN => 0)
     return Rt
 end
-
-A1(n) = 3.084635 - 6.531194n + 8.357854n^2 - 5.082751n^3 + 1.171382n^4 # for n<1
-A2(n) = 504.332889 - 2641.00214n + 5923.699064n^2 - 7376.355814n^3 + 5507.53041n^4 - 2463.357945n^5 + 610.956547n^6 - 64.8047n^7 #for n>1
-
-# seems to work for range 0.5<n<1.8 
-
-
 
 
 function DT_refl_paralpip(t, β, rd, rs, L)
@@ -206,21 +199,21 @@ function DT_refl_paralpip(t, β, rd, rs, L)
 
 	xs = -10:10
 
-	x₁ₗ = Float64[2l*lx + 4l*zb + xu for l in xs]
-	x₂ₗ = Float64[2l*lx + (4l-2)*zb - xu for l in xs]
-	y₁ₘ = Float64[2m*ly + 4m*zb + yu for m in xs]
-	y₂ₘ = Float64[2m*ly + (4m-2)*zb - yu for m in xs]
-	z₁ₙ = Float64[2n*lz + 4n*zb + zo for n in xs]
-	z₂ₙ = Float64[2n*lz + (4n-2)*zb - zo for n in xs]
+	x1l = Float64[2l*lx + 4l*zb + xu for l in xs]
+	x2l = Float64[2l*lx + (4l-2)*zb - xu for l in xs]
+	y1m = Float64[2m*ly + 4m*zb + yu for m in xs]
+	y2m = Float64[2m*ly + (4m-2)*zb - yu for m in xs]
+	z1n = Float64[2n*lz + 4n*zb + zo for n in xs]
+	z2n = Float64[2n*lz + (4n-2)*zb - zo for n in xs]
 
 
 	Rt1 = @. exp(-μa*ν*t)/(2*(4π*D*ν)^(3/2)*t^(5/2))
 
-	Rt2 = @. exp(-(x-x₁ₗ')^2 / (4D*ν*t)) - exp(-(x-x₂ₗ')^2 / (4D*ν*t))
+	Rt2 = @. exp(-(x-x1l')^2 / (4D*ν*t)) - exp(-(x-x2l')^2 / (4D*ν*t))
 
-	Rt3 = @. exp(-(y-y₁ₘ')^2 / (4D*ν*t)) - exp(-(y-y₂ₘ')^2 / (4D*ν*t))
+	Rt3 = @. exp(-(y-y1m')^2 / (4D*ν*t)) - exp(-(y-y2m')^2 / (4D*ν*t))
 
-	Rt4 = @. z₁ₙ'*exp(-z₁ₙ'^2 /(4D*ν*t)) - z₂ₙ'*exp(-z₂ₙ'^2 /(4D*ν*t))
+	Rt4 = @. z1n'*exp(-z1n'^2 /(4D*ν*t)) - z2n'*exp(-z2n'^2 /(4D*ν*t))
 
 	Rt = Rt1.*sum(Rt2, dims=2).*sum(Rt3, dims=2).*sum(Rt4, dims=2)
 	replace!(Rt, NaN =>0)
@@ -270,21 +263,21 @@ function DT_trans_paralpip(t, β, rd, rs, L)
 
 	xs = -10:10
 
-	x₁ₗ = Float64[2l*lx + 4l*zb + xu for l in xs]
-	x₂ₗ = Float64[2l*lx + (4l-2)*zb - xu for l in xs]
-	y₁ₘ = Float64[2m*ly + 4m*zb + yu for m in xs]
-	y₂ₘ = Float64[2m*ly + (4m-2)*zb - yu for m in xs]
-	z₁ₙ = Float64[2n*lz + 4n*zb + zo for n in xs]
-	z₂ₙ = Float64[2n*lz + (4n-2)*zb - zo for n in xs]
+	x1l = Float64[2l*lx + 4l*zb + xu for l in xs]
+	x2l = Float64[2l*lx + (4l-2)*zb - xu for l in xs]
+	y1m = Float64[2m*ly + 4m*zb + yu for m in xs]
+	y2m = Float64[2m*ly + (4m-2)*zb - yu for m in xs]
+	z1n = Float64[2n*lz + 4n*zb + zo for n in xs]
+	z2n = Float64[2n*lz + (4n-2)*zb - zo for n in xs]
 
 
 	Rt1 = @. exp(-μa*ν*t)/(2*(4π*D*ν)^(3/2)*t^(5/2))
 
-	Rt2 = @. exp(-(x-x₁ₗ')^2 / (4D*ν*t)) - exp(-(x-x₂ₗ')^2 / (4D*ν*t))
+	Rt2 = @. exp(-(x-x1l')^2 / (4D*ν*t)) - exp(-(x-x2l')^2 / (4D*ν*t))
 
-	Rt3 = @. exp(-(y-y₁ₘ')^2 / (4D*ν*t)) - exp(-(y-y₂ₘ')^2 / (4D*ν*t))
+	Rt3 = @. exp(-(y-y1m')^2 / (4D*ν*t)) - exp(-(y-y2m')^2 / (4D*ν*t))
 
-	Rt4 = @. (lz - z₁ₙ')*exp(-(lz-z₁ₙ')^2 /(4D*ν*t)) - (lz - z₂ₙ')*exp(-(lz-z₂ₙ')^2 /(4D*ν*t))
+	Rt4 = @. (lz - z1n')*exp(-(lz-z1n')^2 /(4D*ν*t)) - (lz - z2n')*exp(-(lz-z2n')^2 /(4D*ν*t))
 
 	Rt = Rt1.*sum(Rt2, dims=2).*sum(Rt3, dims=2).*sum(Rt4, dims=2)
 	replace!(Rt, NaN =>0)
