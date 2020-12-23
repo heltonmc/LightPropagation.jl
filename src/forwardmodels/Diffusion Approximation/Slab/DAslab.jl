6db346b274117c1a6c1a91b8382f7dd4fd830d89
heltonmc <heltonmc@protonmail.com>
2020-12-22 19:38:09 -0500

    add cw fluence

--- src/forwardmodels/Diffusion Approximation/Slab/DAslab.jl
+++ src/forwardmodels/Diffusion Approximation/Slab/DAslab.jl
@@ -257,3 +257,74 @@ function trans_DA_slab_TD(t, β::Array{Float64,1}, ρ::Float64,ndet::Float64, nm
 
 	return Rt1
 end
+
+
+##### Diffusion approximation for Slab Geometry (CW-fluence) #####
+
+"""
+    fluence_DA_slab_CW(ρ::Float64, β::Array{Float64,1}, ndet::Float64, nmed::Float64, s::Float64, z::Float64)
+
+Compute the steady-state fluence from a slab geometry (x,y->inf, z-> finite). 
+
+# Arguments
+- `ρ::Float64`: the source detector separation (cm⁻¹)
+- `β::Array{Float64,1}`: the optical properties [μa, μs'] (cm⁻¹)
+- `ndet::Float64`: the boundary's index of refraction (air or detector)
+- `nmed::Float64`: the sample medium's index of refraction
+- `s::Float64`: the thickness (z-depth) of the slab (cm)
+- `z::Float64`: the z-depth within slab (cm)
+
+
+
+
+# Examples
+
+julia> fluence_DA_slab_CW(1.0, [0.1,10.], 1.0,1.0, 2.0, 0.)
+
+"""
+function fluence_DA_slab_CW(ρ::Float64, β::Array{Float64,1}, ndet::Float64, nmed::Float64, s::Float64, z::Float64)
+      
+      n::Float64 = nmed/ndet
+      μa::Float64 = β[1]
+      μsp::Float64 = β[2]
+      D::Float64 = 1/3μsp
+      μeff::Float64 = sqrt(3*μa*μsp)
+      xs::UnitRange{Int64} = -10:10
+
+
+
+	  ϕ = 0.0
+
+
+		if n == 1.0
+			A= 1.0
+		elseif n > 1.0
+			A = 504.332889 - 2641.00214n + 5923.699064n^2 - 7376.355814n^3 +
+			5507.53041n^4 - 2463.357945n^5 + 610.956547n^6 - 64.8047n^7
+		else
+			A = 3.084635 - 6.531194n + 8.357854n^2 - 5.082751n^3
+		end
+
+
+		zs::Float64 = 1/μsp
+		ze::Float64 = 2A*D
+
+			
+        Threads.@threads for m in xs 
+
+			zmp = 2m.*(s + 2*ze) .+ zs
+			zmm = 2m.*(s + 2*ze) .- 2*ze .- zs
+
+            ϕ += exp(-μeff*sqrt(ρ^2 + (z - zmp)^2))/(sqrt(ρ^2 + (z - zmp)^2))
+            ϕ -= exp(-μeff*sqrt(ρ^2 + (z - zmm)^2))/(sqrt(ρ^2 + (z - zmm)^2))
+        end	
+
+
+        if isnan(ϕ)
+            ϕ = 0
+        end
+             
+
+
+	return ϕ/(4*π*D)
+end
