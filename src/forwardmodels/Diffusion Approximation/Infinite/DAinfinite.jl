6b6c8b851683b23323206d117db7bd1281cae8dd
Michael Helton <heltonmc@protonmail.com>
2021-07-18 23:13:50 -0400

    fix inf types

--- src/forwardmodels/Diffusion Approximation/Infinite/DAinfinite.jl
+++ src/forwardmodels/Diffusion Approximation/Infinite/DAinfinite.jl
@@ -1,3 +1,30 @@
+#####################################################################################################################################
+# Implements solution to the diffusion equation in an infinite medium as given in [1].
+# Solutions are given in the spatial, frequency, and time domains for a point source located in the middle of the cylinder top.
+#
+# [1] Patterson et. al., "Time resolved reflectance and transmittance for the noninvasive measurement of tissue optical properties," 
+#     Appl. Opt. 28, 2331-2336 (1989)
+#####################################################################################################################################
+
+"""
+    fluence_DA_inf_CW(ρ, μa, μsp)
+
+Compute the steady-state fluence in an infinite medium. 
+
+# Arguments
+- `ρ`: source-detector separation (cm⁻¹)
+- `μa`: absorption coefficient (cm⁻¹)
+- `μsp`: reduced scattering coefficient (cm⁻¹)
+
+# Examples
+julia> fluence_DA_inf_CW(1.0, 0.1, 10.0)
+"""
+function fluence_DA_inf_CW(ρ, μa, μsp)
+    @assert ρ > zero(eltype(ρ)) "ρ must be greater than zero"
+    D = D_coeff(μsp, μa)
+    return exp(-sqrt(3 * μsp * μa) * ρ) / (4 * π * ρ * D)
+end
+
 """
     fluence_DA_inf_TD(t, β::Array{Float64,1}, ρ::Float64, nmed::Float64)
 
@@ -5,139 +32,52 @@ Compute the time-domain fluence in an infinite medium with Eqn. 3 of Patterson. 
 
 # Arguments
 - `t`: the time vector (ns). 
-- `β::Array{Float64,1}`: the optical properties [μa, μs'] (cm⁻¹)
-- `ρ::Float64`: the source detector separation (cm⁻¹)
-- `nmed::Float64`: the sample medium's index of refraction
+- `ρ`: source-detector separation (cm⁻¹)
+- `μa`: absorption coefficient (cm⁻¹)
+- `μsp`: reduced scattering coefficient (cm⁻¹)
+- `ω`: modulation frequency (1/ns)
+- `n_med::Float64`: medium's index of refraction
 
 # Examples
-julia> fluence_DA_inf_TD(0:1:5, [0.1,10.0], 1.0, 1.0)
+julia> fluence_DA_inf_TD(0.1:0.5:5.0, 1.0, 0.1, 10.0)
 """
-function fluence_DA_inf_TD(t, β::Array{Float64,1}, ρ::Float64, nmed::Float64 = 1.0)
-    μa::Float64 = β[1]
-    μsp::Float64 = β[2]
-    D::Float64 = 1/3μsp
-    ν::Float64 = 29.9792458/nmed
+function fluence_DA_inf_TD(t, ρ, μa, μsp, n_med = 1.0)
+    @assert ρ > zero(eltype(ρ))
+    D = D_coeff(μsp, μa)
+    ν = ν_coeff(n_med)
 
-    ϕ = Array{Float64}(undef, length(t))
+    ϕ = Array{eltype(ρ)}(undef, length(t))
 
-    Threads.@threads for n in eachindex(t)
-
-        ϕ[n] = -(ρ^2/(4D*ν*t[n]))
-        ϕ[n] = ϕ[n] - μa*ν*t[n]
-        ϕ[n] = ν*exp(ϕ[n])/((4π*D*ν*t[n])^(3/2))
-
-        if isnan(ϕ[n])
-            ϕ[n] = 0
-        end
- 
+    for n in eachindex(t)
+        @assert t[n] > zero(eltype(t)) "t must be greater than zero"
+        tmp1 = 4 * D * ν * t[n]
+        ϕ[n] = exp(-(ρ^2 / tmp1) - μa * ν * t[n])
+        ϕ[n] *= ν / ((tmp1 * π )^(3 / 2))
     end
 
     return ϕ
 end
 
 """
-    fluence_DA_inf_CW(ρ::Float64, β::Array{Float64,1})
+    fluence_DA_inf_FD(ρ, μa, μsp, ω, n_med)
 
-Compute the fluence for a steady-state source in an infinite medium. 
+Compute the fluence for a frequency modulated source in an infinite medium. 
 
 # Arguments
-- `ρ::Float64`: the source detector separation (cm⁻¹)
-- `β::Array{Float64,1}`: the optical properties [μa, μs'] (cm⁻¹)
+- `ρ`: source-detector separation (cm⁻¹)
+- `μa`: absorption coefficient (cm⁻¹)
+- `μsp`: reduced scattering coefficient (cm⁻¹)
+- `ω`: modulation frequency (1/ns)
+- `n_med::Float64`: medium's index of refraction
 
 # Examples
-julia> fluence_DA_inf_CW(1.0, [0.1,10.0])
+julia> fluence_DA_inf_FD(1.0, 0.1, 10.0, 1.0)
 """
-function fluence_DA_inf_CW(ρ::Float64, β::Array{Float64,1})
-    μa::Float64 = β[1]
-    μsp::Float64 = β[2]
-    D::Float64 = 1/3μsp
+function fluence_DA_inf_FD(ρ, μa, μsp, ω, n_med = 1.0)
+    @assert ρ > zero(eltype(ρ)) "ρ must be greater than zero"
+    D = D_coeff(μsp, μa)
+    ν = ν_coeff(n_med)
 
-    ϕ = exp(-sqrt(3*μsp*μa)*ρ)/(4*π*ρ*D)
-
-    if isnan(ϕ)
-        ϕ = 0
-    end
-
+    ϕ = exp(-ρ * sqrt(μa / D + im * ω / (ν * D))) / (4 * π * ρ * D)
     return ϕ
-end
-
-"""
-    fluence_DA_inf_FD(ρ::Float64, β::Array{Float64,1})
-
-Compute the fluence for a steady-state source in an infinite medium. 
-
-# Arguments
-- `ρ::Float64`: the source detector separation (cm⁻¹)
-- `β::Array{Float64,1}`: the optical properties [μa, μs'] (cm⁻¹)
-
-# Examples
-julia> fluence_DA_inf_FD(1.0, [0.1,10.0])
-"""
-function fluence_DA_inf_FD(ρ, β, ω, nmed = 1.0)
-    μa = β[1]
-    μsp = β[2]
-    D = 1/3μsp
-    ν = 29.9792458/nmed
-
-    ϕ = exp(-ρ*sqrt(μa/D + im*ω/(ν*D)))/(4*π*ρ*D)
-
-    return ϕ
-end
-
-using FastGaussQuadrature
-
-function computeTD_fromFD(t, β, ρ, ub, N)	
-
-    Rt = zeros(Float64, length(t))
-    
-    x, w = gausslegendre(N)
-	
-    Threads.@threads for n in eachindex(t)
-        for m in eachindex(x)
-            Rt[n] += real(exp(im*t[n]*(x[m]*ub/2 +ub/2)))*real(fluence_DA_inf_FD(ρ, [β[1],β[2]], x[m]*ub/2 +ub/2))*w[m]*ub/pi
-        end
-    end
-   
-   
-return Rt
-end
-
-
-function computeTD_fromFD(t, β, ρ, ub, N)	
-
-    Rt = zeros(Float64, length(t))
-    
-    x, w = gausslegendre(N)
-	
-    Threads.@threads for n in eachindex(t)
-        for m in eachindex(x)
-            Rt[n] += real(exp(im*t[n]*(x[m]*ub/2 +ub/2)))*real(fluence_DA_inf_FD(ρ, [β[1],β[2]], x[m]*ub/2 +ub/2))*w[m]*ub/pi
-        end
-    end
-   
-   
-return Rt
-end
-
-
-function computeTD_fromFD2(t, β, ρ, ub, N)	
-
-    Rt = zeros(Float64, length(t))
-    freqcomp = zeros(Float64, N)
-    x, w = gausslegendre(N)
-
-
-    Threads.@threads for m in eachindex(x)
-        freqcomp[m] = real(fluence_DA_inf_FD(ρ, [β[1],β[2]], x[m]*ub/2 +ub/2))*w[m]*ub/pi
-    end
-
-	
-    Threads.@threads for n in eachindex(t)
-        for m in eachindex(x)
-            Rt[n] += real(exp(im*t[n]*(x[m]*ub/2 +ub/2)))*freqcomp[m]
-        end
-    end
-   
-   
-return Rt
 end
