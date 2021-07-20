4c58393d8422a0e0fbb6803b90f19d376185d67a
Michael Helton <heltonmc@protonmail.com>
2021-07-20 10:29:59 -0400

    fix flux def

--- src/forwardmodels/Diffusion Approximation/Slab/DAslab.jl
+++ src/forwardmodels/Diffusion Approximation/Slab/DAslab.jl
@@ -16,6 +16,7 @@
     tmp3 = sqrt(ρ^2 + (z - zmm)^2)
     ϕ = exp(-μeff * tmp2) / tmp2
     ϕ -= exp(-μeff * tmp3) / tmp3
+	return ϕ
 end
 function fluence_DA_slab_CW(ρ, μa, μsp, n_det, n_med, s, z; rtol = 1e-20, maxiter = 1e5)
     D = D_coeff(μsp, μa)
@@ -104,7 +105,7 @@ function fluence_DA_slab_TD(t::AbstractArray, ρ, μa, μsp, n_det, n_med, s, z;
     zb = zb_coeff(A, D)
 
     ϕ = zeros(eltype(ρ), length(t))
-    for ind in eachindex(t)
+    for ind in eachindex(t) # can use @threads or @batch here: @threads better for large t dims
     	ϕ[ind] = _kernel_fluence_DA_slab_TD(D, ν, t[ind], ρ, μa, s, zb, z0, z, maxiter, rtol)
     end
     return ϕ
@@ -131,134 +132,88 @@ Compute the time-domain fluence from a slab geometry (x, y -> inf, z -> finite).
 julia> fluence_DA_slab_TD(0.1:0.1:1.0, 1.0, 0.1, 10.0, 1.0, 1.0, 40.0, 0.0)
 """ fluence_DA_slab_TD
 
-
 ### Reflectance TD ###
 """
-    refl_DA_slab_TD(t, β::Array{Float64,1}, ρ::Float64,ndet::Float64, nmed::Float64, s::Float64)
+    refl_DA_slab_TD(t, ρ, μa, μsp, n_det, n_med, s; xs)
 
 Compute the time-domain reflectance (flux) from a slab geometry (x,y->inf, z-> finite). 
 
 # Arguments
 - `t`: the time vector (ns). 
-- `β::Array{Float64,1}`: the optical properties [μa, μs'] (cm⁻¹)
-- `ρ::Float64`: the source detector separation (cm⁻¹)
-- `ndet::Float64`: the boundary's index of refraction (air or detector)
-- `nmed::Float64`: the sample medium's index of refraction
-- `s::Float64`: the thickness (z-depth) of the slab (cm)
+- `μa`: absorption coefficient (cm⁻¹)
+- `μsp`: reduced scattering coefficient (cm⁻¹)
+- `ρ`: the source detector separation (cm⁻¹)
+- `ndet`: the boundary's index of refraction (air or detector)
+- `nmed`: the sample medium's index of refraction
+- `s`: the thickness (z-depth) of the slab (cm)
 
 # Examples
-julia> refl_DA_slab_TD(0:1:5, [0.1,10.0], 1.0, 1.0, 1.0, 2.0)
+julia> refl_DA_slab_TD(1.0, 1.0, 0.1, 10.0, 1.0, 1.0, 1.0)
 """
-function refl_DA_slab_TD(t, β, ρ,ndet, nmed, s)
-	n = nmed/ndet
-	μa = β[1]
-    μsp = β[2]
-    D = 1/3μsp
-    ν = 29.9792458/nmed
-    xs = -10:10
+function refl_DA_slab_TD(t, ρ, μa, μsp, n_det, n_med, s; xs = -15:15)
+	D = D_coeff(μsp, μa)
+    A = get_afac(n_med / n_det)
+    ν = ν_coeff(n_med)
 
+    z0 = z0_coeff(μsp)
+    zb = zb_coeff(A, D)
+    
     Rt1 = Array{eltype(ρ)}(undef, length(t))
-	Rt2 = zeros(eltype(ρ), length(t))
-
-	@inbounds begin
-
-		if n == 1.0
-			A= 1.0
-		elseif n > 1.0
-			A = 504.332889 - 2641.00214n + 5923.699064n^2 - 7376.355814n^3 +
-	 		5507.53041n^4 - 2463.357945n^5 + 610.956547n^6 - 64.8047n^7
-		else
-			A = 3.084635 - 6.531194n + 8.357854n^2 - 5.082751n^3
-		end
-
-
-		zs = 1/μsp
-		ze = 2A*D
-
-			
-        Threads.@threads for n in eachindex(t) 
-            Rt1[n] = @. -exp(-(ρ^2/(4D*ν*t[n])) - μa*ν*t[n])/(2*(4π*D*ν)^(3/2)*t[n]^(5/2))
-
-			for m in xs
-
-				z3m = -2m*s - 4m*ze - zs
-				z4m = -2m*s - (4m - 2)*ze + zs
-
-	 			Rt2[n] += z3m*exp(-(z3m^2 / (4D*ν*t[n]))) - z4m*exp(-(z4m^2 / (4D*ν*t[n])))
-			end
-					
-			Rt1[n] *= Rt2[n]
-
-            if isnan(Rt1[n])
-                Rt1[n] = 0
-            end
-		    end
-		end
+    Rt2 = zeros(eltype(ρ), length(t))
+	
+    Threads.@threads for n in eachindex(t) 
+        Rt1[n] = -exp(-(ρ^2 / (4 * D * ν * t[n])) - μa * ν * t[n]) / (2 * (4 * π * D * ν)^(3/2) * t[n]^(5/2))
+	    for m in xs
+	        z3m = -2 * m * s - 4 * m * zb - z0
+	        z4m = -2 * m * s - (4m - 2) * zb + z0
+	    	Rt2[n] += z3m * exp(-(z3m^2 / (4 * D * ν * t[n]))) - z4m * exp(-(z4m^2 / (4 * D * ν * t[n])))
+    	end		
+	    Rt1[n] *= Rt2[n]   
+	end
 
 	return Rt1
 end
 
 ### Transmittance TD ###
 """
-    trans_DA_slab_TD(t, β::Array{Float64,1}, ρ::Float64,ndet::Float64, nmed::Float64, s::Float64)
+    trans_DA_slab_TD(t, ρ, μa, μsp, n_det, n_med, s; xs)
 
-Compute the time-domain transmittance from a slab geometry (x,y->inf, z-> finite) with Eqn. 39 from Contini 97. 
+Compute the time-domain transmittance (flux) from a slab geometry (x,y -> inf, z -> finite) with Eqn. 39 from Contini 97. 
 
 # Arguments
 - `t`: the time vector (ns). 
-- `β::Array{Float64,1}`: the optical properties [μa, μs'] (cm⁻¹)
-- `ρ::Float64`: the source detector separation (cm⁻¹)
-- `ndet::Float64`: the boundary's index of refraction (air or detector)
-- `nmed::Float64`: the sample medium's index of refraction
-- `s::Float64`: the thickness (z-depth) of the slab (cm)
+- `μa`: absorption coefficient (cm⁻¹)
+- `μsp`: reduced scattering coefficient (cm⁻¹)
+- `ρ`: the source detector separation (cm⁻¹)
+- `ndet`: the boundary's index of refraction (air or detector)
+- `nmed`: the sample medium's index of refraction
+- `s`: the thickness (z-depth) of the slab (cm)
 
 # Examples
-julia> trans_DA_slab_TD(0:1:5, [0.1,10.0], 1.0, 1.0, 1.0, 2.0)
+julia> trans_DA_slab_TD(1.0, 1.0, 0.1, 10.0, 1.0, 1.0, 1.0)
 """
-function trans_DA_slab_TD(t, β, ρ, ndet, nmed, s)
-	  n = nmed/ndet
-      μa = β[1]
-      μsp = β[2]
-      D = 1/3μsp
-      ν = 29.9792458/nmed
-      xs = -10:10
+function trans_DA_slab_TD(t, ρ, μa, μsp, n_det, n_med, s; xs = -15:15)
+	D = D_coeff(μsp, μa)
+    A = get_afac(n_med / n_det)
+    ν = ν_coeff(n_med)
 
-      Rt1 = Array{eltype(ρ)}(undef, length(t))
-	  Rt2 = zeros(eltype(ρ), length(t))
+    z0 = z0_coeff(μsp)
+    zb = zb_coeff(A, D)
 
-		@inbounds begin
+    Rt1 = Array{eltype(ρ)}(undef, length(t))
+    Rt2 = zeros(eltype(ρ), length(t))
 
-			if n == 1.0
-				A= 1.0
-			elseif n > 1.0
-				A = 504.332889 - 2641.00214n + 5923.699064n^2 - 7376.355814n^3 +
-		 		5507.53041n^4 - 2463.357945n^5 + 610.956547n^6 - 64.8047n^7
-			else
-				A = 3.084635 - 6.531194n + 8.357854n^2 - 5.082751n^3
-			end
+	Threads.@threads for n in eachindex(t) 
+        Rt1[n] = exp(-(ρ^2 / (4 * D * ν * t[n])) - μa * ν * t[n]) / (2 * (4 * π * D * ν)^(3/2) * t[n]^(5/2))
+        for m in xs
+            z1m = (1 - 2 * m) * s - 4 * m * zb - z0
+		    z2m = (1 - 2 * m) * s - (4 * m - 2) * zb + z0
 
-			zs = 1/μsp
-			ze = 2A*D
-	
-                Threads.@threads for n in eachindex(t) 
-                    Rt1[n] = @. exp(-(ρ^2/(4D*ν*t[n])) - μa*ν*t[n])/(2*(4π*D*ν)^(3/2)*t[n]^(5/2))
-
-					for m in xs
-
-						z1m = (1 - 2*m)*s - 4m*ze - zs
-						z2m = (1 - 2*m)*s - (4m - 2)*ze + zs
-
-			 			Rt2[n] += z1m*exp(-(z1m^2 / (4D*ν*t[n]))) - z2m*exp(-(z2m^2 / (4D*ν*t[n])))
-					end
-					
-					Rt1[n] *= Rt2[n]
-
-                    if isnan(Rt1[n])
-                        Rt1[n] = 0
-                    end
-				end
-			end
-
+		    Rt2[n] += z1m * exp(-(z1m^2 / (4 * D * ν * t[n]))) - z2m * exp(-(z2m^2 / (4 * D * ν * t[n])))
+	    end
+	    Rt1[n] *= Rt2[n]
+    end
+			
 	return Rt1
 end
 
