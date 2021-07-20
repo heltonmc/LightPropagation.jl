392c13d2b70b21a4431c1b6f428a18b549dc3cf9
Michael Helton <heltonmc@protonmail.com>
2021-07-20 14:38:23 -0400

    switch to new inputs

--- test/DAparalpipTests/runtests.jl
+++ test/DAparalpipTests/runtests.jl
@@ -11,11 +11,11 @@ using LightPropagation: fluence_DA_paralpip_TD
 t = 0.1:0.5:8
 
 # compare against semi-inf
-@test fluence_DA_paralpip_TD(t, [0.1,10.0], 1.0, 1.0, [4.0,5.0,0.0], [5.0,5.0], [10.0,10.0,10.0]) ≈ fluence_DA_semiinf_TD(t, [0.1, 10.0], 1.0, 1.0, 1.0, 0.0)  
-@test fluence_DA_paralpip_TD(t, [0.1,70.0], 1.0, 1.0, [2.0,5.0,0.0], [5.0,5.0], [10.0,10.0,10.0]) ≈ fluence_DA_semiinf_TD(t, [0.1, 70.0], 3.0, 1.0, 1.0, 0.0)  
+@test fluence_DA_paralpip_TD(t, 0.1, 10.0, 1.0, 1.0, [4.0,5.0,0.0], [5.0,5.0], [10.0,10.0,10.0]) ≈ fluence_DA_semiinf_TD(t, 1.0, 0.1, 10.0, 1.0, 1.0, 0.0)  
+@test fluence_DA_paralpip_TD(t, 0.1, 70.0, 1.0, 1.0, [2.0,5.0,0.0], [5.0,5.0], [10.0,10.0,10.0]) ≈ fluence_DA_semiinf_TD(t, 3.0, 0.1, 70.0, 1.0, 1.0, 0.0)  
 
 # compare against slabs 
-@test fluence_DA_paralpip_TD(t, [0.1,10.0], 1.0, 1.0, [4.0,5.0,0.0], [5.0,5.0], [10.0,10.0,10.0])  ≈ fluence_DA_slab_TD(t, [0.1, 10.0], 1.0, 1.0, 1.0, 10.0, 0.0)
-@test fluence_DA_paralpip_TD(t, [0.1,10.0], 1.0, 1.0, [4.0,5.0,0.0], [5.0,5.0], [10.0,10.0,0.5])  ≈ fluence_DA_slab_TD(t, [0.1, 10.0], 1.0, 1.0, 1.0, 0.5, 0.0)
+@test fluence_DA_paralpip_TD(t, 0.1, 10.0, 1.0, 1.0, [4.0,5.0,0.0], [5.0,5.0], [10.0,10.0,10.0])  ≈ fluence_DA_slab_TD(t, 1.0, 0.1, 10.0, 1.0, 1.0, 10.0, 0.0)
+@test fluence_DA_paralpip_TD(t, 0.1, 10.0, 1.0, 1.0, [4.0,5.0,0.0], [5.0,5.0], [10.0,10.0,0.5])  ≈ fluence_DA_slab_TD(t, 1.0, 0.1, 10.0, 1.0, 1.0, 0.5, 0.0)
 
 end # module
