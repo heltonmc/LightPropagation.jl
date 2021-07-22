d6bf29fa47e3396a12efdcf827cca39cfbe7ac5a
Michael Helton <heltonmc@protonmail.com>
2021-07-21 20:08:45 -0400

    Update versions.js

--- versions.js
+++ versions.js
@@ -1,10 +1,9 @@
 var DOC_VERSIONS = [
   "stable",
-  "v1.1",
   "v0.4",
   "v0.3",
   "v0.2",
   "dev",
 ];
-var DOCUMENTER_NEWEST = "v1.1";
+var DOCUMENTER_NEWEST = "v0.4";
 var DOCUMENTER_STABLE = "stable";
