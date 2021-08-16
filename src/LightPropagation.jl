e13dbb02fbe931ab50b52069c2651aa5b5848ec9
Michael Helton <heltonmc@protonmail.com>
2021-08-16 14:26:56 -0400

    export flux

--- src/LightPropagation.jl
+++ src/LightPropagation.jl
@@ -8,29 +8,51 @@ using FFTW
 using Parameters
 using SpecialFunctions
 using JLD
+using ForwardDiff
 
 export TPSF_DA_semiinf_refl
 export TPSF_DA_slab_refl
 export TPSF_DA_paralpip_refl
 
+### Diffusion Theory
+
+# Infinite
 export fluence_DA_inf_CW
 export fluence_DA_inf_TD
+
+# Semi-infinite
 export fluence_DA_semiinf_TD
 export fluence_DA_semiinf_CW
+
+export flux_DA_semiinf_CW
+export flux_DA_semiinf_TD
+
+# Slab
 export fluence_DA_slab_CW
 export fluence_DA_slab_TD
+
+export flux_DA_slab_CW
+export flux_DA_slab_TD
+
+# Parallelepiped
 export fluence_DA_paralpip_TD
 export fluence_DA_paralpip_CW
 
-export Nlayer_cylinder
+# Layered cylinder
 export fluence_DA_Nlay_cylinder_CW
 export fluence_DA_Nlay_cylinder_TD
+
+export flux_DA_Nlay_cylinder_CW
+export flux_DA_Nlay_cylinder_TD
+
+# Structures
+export Nlayer_cylinder
+
+# constants
 export besselroots
 
 export getfit
-
 export load_asc_data
-
 export fitDTOF, DTOF_fitparams
 
 include("forwardmodels/Diffusion Approximation/diffusionparameters.jl")
