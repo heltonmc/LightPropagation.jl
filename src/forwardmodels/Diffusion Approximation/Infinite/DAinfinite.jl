aaa003fd3581f658eb78fc1cacff245b27e11844
Michael Helton <heltonmc@protonmail.com>
2021-10-12 12:23:59 -0400

    update structure to infinite

--- src/forwardmodels/Diffusion Approximation/Infinite/DAinfinite.jl
+++ src/forwardmodels/Diffusion Approximation/Infinite/DAinfinite.jl
@@ -1,14 +1,60 @@
-#####################################################################################################################################
+#------------------------------------------------------------------------------------------------------------------------
 # Implements solution to the diffusion equation in an infinite medium as given in [1].
 # Solutions are given in the steady-state, frequency, and time domains for an isotroptic source.
 #
 # [1] Patterson et. al., "Time resolved reflectance and transmittance for the noninvasive measurement of tissue optical properties," 
 #     Appl. Opt. 28, 2331-2336 (1989)
-#####################################################################################################################################
+#-------------------------------------------------------------------------------------------------------------------------
+""" Structure containing inputs for simulating the fluence under the diffusion approximation in the infinite space."""
+struct DAinfinite{T <: AbstractFloat} <: DiffusionParameters
+    ρ::T                                 # distance away from isotropic point source (cm)
+    μa::T                                # absorption coefficient (cm⁻¹)
+    μsp::T                               # reduced scattering coefficient (cm⁻¹)
+    n_med::T                             # medium's index of refraction
 
-#####################################
+    t::Union{T, AbstractVector{T}}       # time (ns)
+    ω::T                                 # modulation frequency (1/ns)
+
+    # do not need to provide these arguments (calculated from previous inputs)
+    
+    D::T                                 # Diffusion coefficient                        
+    ν::T                                 # Speed of light in medium (cm/ns)
+    function DAinfinite{T}(ρ::T, μa::T, μsp::T, n_med::T, t::Union{T, AbstractVector{T}}, ω::T) where {T <: AbstractFloat}
+        @assert ρ > zero(T) "ρ must be positive"
+        @assert μa >= zero(T) "μa must greater than or equal to 0"
+        @assert μsp > zero(T) "μsp must greater than 0"
+        @assert all(t .> zero(T)) "t must be greater than 0"
+        return new{T}(ρ, μa, μsp, n_med, t, ω, D_coeff(μsp), ν_coeff(n_med))
+    end
+end
+
+"""
+    Generator function for DAinfinite structure.
+
+Provides default parameters to use in the infinite space fluence calculation in either the CW, FD, or TD.
+
+# Examples
+```
+julia> data = DAinfinite() # return default parameters
+julia> data = DAinfinite(ρ = 1.5) # return ρ = 1.5 with the rest of the parameters given by defaults
+julia> fluence_DA_inf_CW(data) # can then be used with corresponding functions
+```
+"""
+function DAinfinite(;
+    ρ::T = 1.0,
+    μa::T = 0.1,
+    μsp::T = 10.0,
+    n_med::T = 1.0,
+    ω::T = 0.0,
+    t::Union{T, AbstractVector{T}} = 1.0
+) where {T<:AbstractFloat}
+    return DAinfinite{T}(ρ, μa, μsp, n_med, t, ω)
+end
+
+#-------------------------------------
 # Steady-State Fluence 
-#####################################
+#-------------------------------------
+
 """
     fluence_DA_inf_CW(ρ, μa, μsp)
 
@@ -20,18 +66,74 @@ Compute the steady-state fluence in an infinite medium.
 - `μsp`: reduced scattering coefficient (cm⁻¹)
 
 # Examples
-julia> `fluence_DA_inf_CW(1.0, 0.1, 10.0)`
+```
+julia> fluence_DA_inf_CW(1.0, 0.1, 10.0)
+```
 """
-function fluence_DA_inf_CW(ρ, μa, μsp)
-    @assert ρ > zero(eltype(ρ)) "ρ must be greater than zero"
-    D = D_coeff(μsp, μa)
-
+function fluence_DA_inf_CW(ρ, μa, μsp; D = D_coeff(μsp))
     return exp(-sqrt(3 * μsp * μa) * ρ) / (4 * π * ρ * D)
 end
+"""
+    fluence_DA_inf_CW(data::DiffusionParameters)
 
-#####################################
+Wrapper to `fluence_DA_inf_CW(ρ, μa, μsp)`
+
+# Examples
+```
+julia> data = DAinfinite(ρ = 1.0) # use structure to generate inputs
+julia> fluence_DA_inf_CW(data) # then call the function
+```
+"""
+function fluence_DA_inf_CW(data::DiffusionParameters)
+    return fluence_DA_inf_CW(data.ρ, data.μa, data.μsp, D = data.D)
+end
+
+#-------------------------------------
+# Frequency-Domain Fluence 
+#-------------------------------------
+"""
+    fluence_DA_inf_FD(ρ, μa, μsp, ω; n_med = 1.0, ν = ν_coeff(n_med), D = D_coeff(μsp))
+
+Compute the fluence for a frequency modulated source in an infinite medium. 
+
+# Arguments
+- `ρ`: source-detector separation (cm⁻¹)
+- `μa`: absorption coefficient (cm⁻¹)
+- `μsp`: reduced scattering coefficient (cm⁻¹)
+- `ω`: modulation frequency (1/ns)
+
+# Optional Arguments
+- `n_med`=1.0: medium's index of refraction
+- `ν`: speed of light in the medium cm/ns
+- `D`: Diffusion coefficient
+
+# Examples
+```
+julia> fluence_DA_inf_FD(1.0, 0.1, 10.0, 1.0, n_med = 1.4)
+```
+"""
+function fluence_DA_inf_FD(ρ, μa, μsp, ω; n_med = 1.0, ν = ν_coeff(n_med), D = D_coeff(μsp))
+    μa_complex = μa + ω * im / ν
+    return fluence_DA_inf_CW(ρ, μa_complex, μsp, D = D)
+end
+"""
+    fluence_DA_inf_FD(data::DiffusionParameters)
+
+Wrapper to `fluence_DA_inf_FD(ρ, μa, μsp, ω; n_med = 1.0, ν = ν_coeff(n_med), D = D_coeff(μsp))`
+
+# Examples
+```
+julia> data = DAinfinite(ω = 1.0) # use structure to generate inputs
+julia> fluence_DA_inf_FD(data) # then call the function
+```
+"""
+function fluence_DA_inf_FD(data::DiffusionParameters)
+    return fluence_DA_inf_FD(data.ρ, data.μa, data.μsp, data.ω, ν = data.ν, D = data.D)
+end
+
+#-------------------------------------
 # Time-Domain Fluence 
-#####################################
+#-------------------------------------
 """
     fluence_DA_inf_TD(t, ρ, μa, μsp; nmed = 1.0)
 
@@ -42,57 +144,47 @@ Compute the time-domain fluence in an infinite medium with Eqn. 3 of Patterson. 
 - `ρ`: source-detector separation (cm⁻¹)
 - `μa`: absorption coefficient (cm⁻¹)
 - `μsp`: reduced scattering coefficient (cm⁻¹)
-- `n_med::Float64`: medium's index of refraction
+
+# Optional Arguments
+- `n_med`=1.0: medium's index of refraction
+- `ν`: speed of light in the medium cm/ns
+- `D`: Diffusion coefficient
 
 # Examples
-julia> `fluence_DA_inf_TD(0.1:0.5:5.0, 1.0, 0.1, 10.0, n_med = 1.4)`
+```
+julia> fluence_DA_inf_TD(0.1:0.5:5.0, 1.0, 0.1, 10.0, n_med = 1.4)
+```
 """
-function fluence_DA_inf_TD(t, ρ, μa, μsp, n_med = 1.0)
-    @assert ρ > zero(eltype(ρ))
-    D = D_coeff(μsp, μa)
-    ν = ν_coeff(n_med)
+function fluence_DA_inf_TD(t::AbstractVector, ρ, μa, μsp; n_med = 1.0, ν = ν_coeff(n_med), D = D_coeff(μsp))
+    T = promote_type(eltype(ρ), eltype(μa), eltype(μsp))
+    ϕ = zeros(T, length(t))
 
-    if isa(t, AbstractFloat)
-        ϕ = _kernel_fluence_DA_inf_TD(t, D, ν, ρ, μa)
-		return ϕ
-	elseif isa(t, AbstractArray)
-		ϕ = zeros(eltype(ρ), length(t))
-        @inbounds Threads.@threads for ind in eachindex(t)
-    		ϕ[ind] = _kernel_fluence_DA_inf_TD(t[ind], D, ν, ρ, μa)
-    	end
-    	return ϕ
-	end
+    @inbounds Threads.@threads for ind in eachindex(t)
+        ϕ[ind] = _kernel_fluence_DA_inf_TD(t[ind], ρ, μa, ν, D)
+    end
 
     return ϕ
 end
-@inline function _kernel_fluence_DA_inf_TD(t, D, ν, ρ, μa)
-    @assert t > zero(eltype(t)) "t must be greater than zero"
+function fluence_DA_inf_TD(t::AbstractFloat, ρ, μa, μsp; n_med = 1.0, ν = ν_coeff(n_med), D = D_coeff(μsp))
+    return _kernel_fluence_DA_inf_TD(t, ρ, μa, ν, D)
+end
+"""
+    fluence_DA_inf_TD(data::DiffusionParameters)
+
+Wrapper to `fluence_DA_inf_TD(ρ, μa, μsp, ω; n_med = 1.0, ν = ν_coeff(n_med), D = D_coeff(μsp))`
+
+# Examples
+```
+julia> data = DAinfinite(t = 0.01:0.1:2.0) # use structure to generate inputs
+julia> fluence_DA_inf_TD(data) # then call the function
+```
+"""
+function fluence_DA_inf_TD(data::DiffusionParameters)
+    return fluence_DA_inf_TD(data.t, data.ρ, data.μa, data.μsp, n_med = data.n_med, ν = data.ν, D = data.D)
+end
+
+@inline function _kernel_fluence_DA_inf_TD(t, ρ, μa, ν, D)
     tmp1 = 4 * D * ν * t
     ϕ = exp(-(ρ^2 / tmp1) - μa * ν * t)
     ϕ *= ν / ((tmp1 * π )^(3/2))
-end
-
-#####################################
-# Frequency-Domain Fluence 
-#####################################
-"""
-    fluence_DA_inf_FD(ρ, μa, μsp, ω; n_med = 1.0)
-
-Compute the fluence for a frequency modulated source in an infinite medium. 
-
-# Arguments
-- `ρ`: source-detector separation (cm⁻¹)
-- `μa`: absorption coefficient (cm⁻¹)
-- `μsp`: reduced scattering coefficient (cm⁻¹)
-- `ω`: modulation frequency (1/ns)
-- `n_med::Float64`: medium's index of refraction
-
-# Examples
-julia> `fluence_DA_inf_FD(1.0, 0.1, 10.0, 1.0, n_med = 1.4)`
-"""
-function fluence_DA_inf_FD(ρ, μa, μsp, ω, n_med = 1.0)
-    ν = ν_coeff(n_med)
-    μa_complex = μa + ω * im / ν
-
-    return fluence_DA_inf_CW(ρ, μa_complex, μsp)
 end
--- src/forwardmodels/Diffusion Approximation/diffusionparameters.jl
+++ src/forwardmodels/Diffusion Approximation/diffusionparameters.jl
@@ -1,4 +1,23 @@
-@inline function A_coeff(n)
+"""
+    abstract type DiffusionParameters
+
+Abstract type for all input structures to diffusion functions.
+"""
+abstract type DiffusionParameters end
+
+"""
+    A_coeff(n)
+
+Function to approximate the A coefficient (mismatch between external and internal index of refractions) given in Eq. 25 from Ref. [1]. 
+n is the ratio between the refractive index of the diffusing medium and that of the external medium (n_med / n_ext).
+Uses the polynomial fit given in Appendix A [1]. Used to calculate the extrapolation length.
+
+References:
+[1] Daniele Contini, Fabrizio Martelli, and Giovanni Zaccanti, 
+    "Photon migration through a turbid slab described by a model based on diffusion approximation. I. Theory," 
+    Appl. Opt. 36, 4587-4599 (1997) 
+"""
+function A_coeff(n)
     if n > 1.0
         A = 504.332889 - 2641.00214n + 5923.699064n^2 - 7376.355814n^3 +
         5507.53041n^4 - 2463.357945n^5 + 610.956547n^6 - 64.8047n^7
@@ -9,30 +28,35 @@
     end
     return A
 end
-@inline D_coeff(μsp, μa) = 1 / (3 * μsp)
-@inline ν_coeff(n_med) = 29.9792458 / n_med
-@inline z0_coeff(μsp) = 1 / μsp
-@inline zb_coeff(A, D) = 2 * A * D
 
-function diffusionparams(μsp, n_med, n_ext)
-    ## Diffusion parameters
-    D = @. 1/3μsp
-    ν = @. 29.9792345/n_med
-    A = @. get_afac(n_ext/n_med) # need to calculate reflection between layers and surrounding medium
-    zb = @. 2*A*D
-    z0 = 1/(μsp[1])
+"""
+    D_coeff(μsp)
 
-    return D, ν, A, zb, z0
-end
+Returns the diffusion coefficient as a function of the reduced scattering coefficient.
+Note that this is sometimes defined with the absorption coefficient D = 1 / (3 * (μsp + μa)).
+"""
+D_coeff(μsp) = 1 / (3 * μsp)
 
-@inline function get_afac(n)
-    if n > 1.0
-        A = 504.332889 - 2641.00214n + 5923.699064n^2 - 7376.355814n^3 +
-        5507.53041n^4 - 2463.357945n^5 + 610.956547n^6 - 64.8047n^7
-    elseif n < 1.0
-        A = 3.084635 - 6.531194n + 8.357854n^2 - 5.082751n^3
-    else 
-        A = 1.0
-    end
-    return A
-end
+"""
+    ν_coeff(n_med)
+
+Computes the speed of light in the medium (cm/ns). 
+This is a good place to change the units of the returning function along with optical properties if interested in mm or ps.
+"""
+ν_coeff(n_med) = 29.9792458 / n_med
+
+"""
+    z0_coeff(μsp)
+
+The location of the isotropic point source within the medium assuming some perpindicular incidence of light beam.
+Note that this is sometimes defined with the absorption coefficient z0 = 1 / (μsp + μa)
+"""
+z0_coeff(μsp) = 1 / μsp
+
+"""
+    zb_coeff(A, D)
+
+The extrapolation length calculated with the A coefficient (accounts for mismatch index of refraction at boundary)
+and D (diffusion coefficient).
+"""
+zb_coeff(A, D) = 2 * A * D
