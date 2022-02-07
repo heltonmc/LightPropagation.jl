49edd1c73455ebce152d8400d803c5c6d3da28d2
Michael Helton <heltonmc@protonmail.com>
2022-02-07 11:13:42 -0600

    run all test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,16 +2,16 @@ module LightPropagationRunTests
 
 using Test
 
-#@time @testset "DAsemiinf" begin include("DAsemiinfTests/runtests.jl") end
+@time @testset "DAsemiinf" begin include("DAsemiinfTests/runtests.jl") end
 
-#@time @testset "DAslab" begin include("DAslabTests/runtests.jl") end
+@time @testset "DAslab" begin include("DAslabTests/runtests.jl") end
 
-#@time @testset "DAparalpip" begin include("DAparalpipTests/runtests.jl") end
+@time @testset "DAparalpip" begin include("DAparalpipTests/runtests.jl") end
 
 @time @testset "DA_Nlayer" begin include("DA_NlayerTests/runtests.jl") end
 
-#@time @testset "DAinf" begin include("DAinfTests/runtests.jl") end
+@time @testset "DAinf" begin include("DAinfTests/runtests.jl") end
 
-#@time @testset "DAdcs" begin include("DA_DCSTests/runtests.jl") end
+@time @testset "DAdcs" begin include("DA_DCSTests/runtests.jl") end
 
 end
