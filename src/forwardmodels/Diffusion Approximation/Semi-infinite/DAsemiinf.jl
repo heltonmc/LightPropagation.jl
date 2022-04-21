28b8d71a561ec098761e70c74c5ede16ab139dc2
Michael Helton <heltonmc@protonmail.com>
2022-04-21 12:38:46 -0400

    update docs, flux

--- src/forwardmodels/Diffusion Approximation/Semi-infinite/DAsemiinf.jl
+++ src/forwardmodels/Diffusion Approximation/Semi-infinite/DAsemiinf.jl
@@ -1,10 +1,9 @@
 #----------------------------------------------------------------------------------------------------------------------------------------
-# Implements solution to the diffusion equation for the fluence in the steady-state and time-domain in a semi-infinite medium [1][2][3].
+# Implements solutions to the diffusion equation for the fluence in the steady-state and time-domain in a semi-infinite medium using
+# equations 1, 3, 5, and 6 from Kienle and Patterson [1].
 #
 # [1] Alwin Kienle and Michael S. Patterson, "Improved solutions of the steady-state and the time-resolved diffusion equations 
 #     for reflectance from a semi-infinite turbid medium," J. Opt. Soc. Am. A 14, 246-254 (1997) 
-# [2] Martelli, Fabrizio, et al. Light propagation through biological tissue and other diffusive media: theory, solutions and software. 
-#     Vol. 10. No. 3.824746. Bellingham: SPIE press, 2010.
 #----------------------------------------------------------------------------------------------------------------------------------------
 """
     fluence_DA_semiinf_CW(ρ, μa, μsp; n_ext = 1.0, n_med = 1.0, z = 0.0)
@@ -38,6 +37,8 @@ Compute the steady-state flux (D*∂ϕ(ρ)/∂z @ z = 0) from a semi-infinite me
 - `ρ`: the source detector separation (cm⁻¹)
 - `μa`: absorption coefficient (cm⁻¹)
 - `μsp`: reduced scattering coefficient (cm⁻¹)
+
+# Keyword arguments
 - `ndet`: the boundary's index of refraction (air or detector)
 - `nmed`: the sample medium's index of refraction
 
@@ -98,7 +99,7 @@ function fluence_DA_semiinf_TD end
 """
     flux_DA_semiinf_TD(t, ρ, μa, μsp; n_ext = 1.0, n_med = 1.0)
 
-Compute the time-domain flux (D*∂ϕ(t)/∂z @ z = 0) from a semi-infinite medium from Eqn. 36 Contini 97 (m = 0).
+Compute the time-domain flux (D*∂ϕ(t)/∂z @ z = 0) in a semi-infinite medium.
 
 # Arguments
 - `t`: the time vector (ns). 
@@ -132,7 +133,6 @@ function _kernel_fluence_DA_semiinf_CW(μeff, ρ, z, z0, zb, D)
     b = hypot(ρ, z + 2 * zb + z0)
 
     # the following tries to compute ϕ = exp(-μeff * a) / a - exp(-μeff * b) / b more accurately
-    # there is some loss of significance when a ≈ b and a > 5
     # h also tries to compute h = b - a more accurately
     h = 4 * (z0 * z + z0 * zb + z * zb + zb^2) / (a + b)
     ϕ = exp(-b * μeff) * fma(b, expm1(h * μeff), h) / (b * a)
@@ -150,8 +150,18 @@ end
 #------------------------------
 
 function flux_DA_semiinf_CW(ρ, μa, μsp; n_ext = 1.0, n_med = 1.0)
-    D = D_coeff(μsp)
-    return D * ForwardDiff.derivative(z -> fluence_DA_semiinf_CW(ρ, μa, μsp, n_med = n_med, n_ext = n_ext, z = z), 0.0)
+    T = eltype(ρ)
+
+    μeff = sqrt(3 * μa * μsp)
+    D, A = D_coeff(μsp), A_coeff(n_med / n_ext)
+    z0, zb = z0_coeff(μsp), zb_coeff(A, D)
+
+    a = hypot(z0, ρ)
+    b = hypot(z0 + 2*zb, ρ)
+    ϕ = z0 * (μeff + inv(a)) * exp(-μeff * a) / a^2
+    ϕ += (z0 + 2*zb) * (μeff + inv(b)) * exp(-μeff * b) / b^2
+
+    return ϕ / (4 * T(π))
 end
 
 #------------------------------
@@ -197,17 +207,18 @@ function flux_DA_semiinf_TD(t, ρ, μa, μsp; n_ext = 1.0, n_med = 1.0)
     D, A, ν = D_coeff(μsp), A_coeff(n_med / n_ext), ν_coeff(n_med)
     z0, zb = z0_coeff(μsp), zb_coeff(A, D)
 
-    z3m = -z0
-    z4m = 2 * zb + z0
+    a = hypot(z0, ρ)^2
+    b = hypot(z0 + 2 * zb, ρ)^2
 
-    return map(t -> _kernel_flux_DA_semiinf_TD(D, ν, t, μa, z3m, z4m, ρ), t)
- end
-@inline function _kernel_flux_DA_semiinf_TD(D, ν, t, μa, z3m, z4m, ρ)
-    tmp1 = 4 * D * ν * t
-    Rt = -(ρ^2 / (tmp1))
-    Rt -= μa * ν * t
-    Rt = -exp(Rt) / (2 * (4 * π * D * ν)^(3/2) * sqrt(t * t * t * t * t))
-    Rt *= (z3m * exp(-(z3m^2 / (tmp1))) - z4m * exp(-(z4m^2 / (tmp1))))
+    return  map(t -> _kernel_flux_DA_semiinf_TD(D, ν, t, μa, a, b, z0, zb), t)
+end
 
-    return Rt
+@inline function _kernel_flux_DA_semiinf_TD(D, ν, t, μa, a, b, z0, zb)
+    tmp = inv(4 * D * ν * t)
+    ϕ = z0 * exp(-a * tmp)
+    ϕ += (z0 + 2 * zb) * exp(-b * tmp)
+    ϕ *= exp(-μa * ν * t)
+    ϕ /= 2 * (4 * D * ν * π)^(3/2) * t^(5/2)
+
+    return ϕ 
 end
